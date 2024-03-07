55f852c96a0e9ebc22a3876208feb9f0b163de10
AngelEspi1 <162532033+AngelEspi1@users.noreply.github.com>
2024-03-07 09:02:56 -0600

    Update CALCULADORA.cpp

--- CALCULADORA.cpp
+++ CALCULADORA.cpp
@@ -6,7 +6,7 @@ void suma(float num1, float num2);
 void resta(float num1, float num2);
 void multiplicacion(float num1, float num2);
 void division(float num1, float num2);
-float Raiz(float numero);
+float Raiz(float num1);
 main (){
 	int num1,num2;
 	char menu;
@@ -14,7 +14,8 @@ main (){
 		cout<<"a) Suma"<<endl;
 		cout<<"b) Resta"<<endl;
 		cout<<"c) Multiplicacion"<<endl;
-		cout<<"d) Raiz cuadrada"<<endl;
+		cout<<"d) Division"<<endl;
+		cout<<"e) Raiz Cuadrada"<<endl;
 		cin>>menu;
 		switch (menu){
 		
@@ -46,6 +47,20 @@ main (){
 	   			cin>>num2;
        				division(num1,num2);
 	   			break;
+			case 'e':
+				cout<<"Dame el valor del numero: ";
+				cin>>num1;
+				Raiz(num1);
+				    if (raiz == -1.0) {
+        cout << "No se puede calcular la raíz cuadrada de un número negativo." << endl;
+    } else {
+        cout << "La raíz cuadrada de " << numero << " es: " << raiz << endl;
+    }
+
+    return 0;
+}
+
+				break;
 		}
 	}
 }
@@ -70,16 +85,16 @@ void division(float num1, float num2){
  	total = num1 / num2;
 	cout<<"El resultado final es:	"<<total;
 }
-float Raiz(float numero) {
+float Raiz(float num1) {
     if (numero < 0) {
         return -1.0;
     }
 
-    float x0 = numero / 2.0; 
+    float x0 = num1 / 2.0; 
     float x1;
 
     do {
-        x1 = (x0 + numero / x0) / 2.0;
+        x1 = (x0 + num1 / x0) / 2.0;
     } while (abs(x1 - x0) >= 0.0001);
 
     return x1;

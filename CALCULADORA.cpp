574692b3ed5b85d75620af68631e480bbc127a20
Rom <160189542+STARKILL117rm@users.noreply.github.com>
2024-03-07 09:34:36 -0600

    Update CALCULADORA.cpp

--- CALCULADORA.cpp
+++ CALCULADORA.cpp
@@ -1,5 +1,5 @@
-
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 void suma(float num1, float num2);
@@ -7,95 +7,104 @@ void resta(float num1, float num2);
 void multiplicacion(float num1, float num2);
 void division(float num1, float num2);
 float raiz(float num1);
-main (){
-	int num1,num2;
-	char menu;
-		cout<<"--> Menu de la calculadora <---"<<endl;
-		cout<<"a) Suma"<<endl;
-		cout<<"b) Resta"<<endl;
-		cout<<"c) Multiplicacion"<<endl;
-		cout<<"d) Division"<<endl;
-		cout<<"e) Raiz Cuadrada"<<endl;
-		cin>>menu;
-		switch (menu){
-		
-			case 'a':
-				cout<<"Dame el valor del numero 1: ";
-				cin>>num1;
-				cout<<"Dame el valor del numero 2: ";
-				cin>>num2;
-				suma(num1,num2);
-				break;
-			case 'b':
-				cout<<"Dame el valor del numero 1: ";
-				cin>>num1;
-				cout<<"Dame el valor del numero 2: ";
-				cin>>num2;
-				resta(num1,num2);
-				break;	
-			case 'c':
-       				cout<<"Dame el valor del numero 1: ";
-	   			cin>>num1;
-       				cout<<"Dame el valor del numero 2: ";
-	   			cin>>num2;
-       				multiplicacion(num1,num2);
-	   			break;
-			case 'd':
-       				cout<<"Dame el valor del numero 1: ";
-	   			cin>>num1;
-       				cout<<"Dame el valor del numero 2: ";
-	   			cin>>num2;
-       				division(num1,num2);
-	   			break;
-			case 'e':
-				cout<<"Dame el valor del numero: ";
-				cin>>num1;
-				raiz(num1);
-				    if (raiz == -1.0) {
-        cout << "No se puede calcular la raíz cuadrada de un número negativo." << endl;
-    } else {
-        cout << "La raíz cuadrada de " << numero << " es: " << raiz << endl;
-    }
+float exponente(float base, float exponente);
 
+int main() {
+    int num1, num2;
+    char menu;
+    cout << "--> Menu de la calculadora <---" << endl;
+    cout << "a) Suma" << endl;
+    cout << "b) Resta" << endl;
+    cout << "c) Multiplicacion" << endl;
+    cout << "d) Division" << endl;
+    cout << "e) Raiz Cuadrada" << endl;
+    cout << "f) Exponente" << endl;
+    cin >> menu;
+    switch (menu) {
+    case 'a':
+        cout << "Dame el valor del numero 1: ";
+        cin >> num1;
+        cout << "Dame el valor del numero 2: ";
+        cin >> num2;
+        suma(num1, num2);
+        break;
+    case 'b':
+        cout << "Dame el valor del numero 1: ";
+        cin >> num1;
+        cout << "Dame el valor del numero 2: ";
+        cin >> num2;
+        resta(num1, num2);
+        break;
+    case 'c':
+        cout << "Dame el valor del numero 1: ";
+        cin >> num1;
+        cout << "Dame el valor del numero 2: ";
+        cin >> num2;
+        multiplicacion(num1, num2);
+        break;
+    case 'd':
+        cout << "Dame el valor del numero 1: ";
+        cin >> num1;
+        cout << "Dame el valor del numero 2: ";
+        cin >> num2;
+        division(num1, num2);
+        break;
+    case 'e':
+        cout << "Dame el valor del numero: ";
+        cin >> num1;
+        raiz(num1);
+        if (raiz == -1.0) {
+            cout << "No se puede calcular la raíz cuadrada de un número negativo." << endl;
+        } else {
+            cout << "La raiz cuadrada de " << num1 << " es: " << raiz(num1) << endl;
+        }
+        break;
+    case 'f':
+        float base, exp;
+        cout << "Dame el valor de la base: ";
+        cin >> base;
+        cout << "Dame el valor del exponente: ";
+        cin >> exp;
+        cout << "El resultado de " << base << " elevado a " << exp << " es: " << exponente(base, exp) << endl;
+        break;
+    default:
+        cout << "Opción no válida." << endl;
+        break;
+    }
     return 0;
 }
 
-				break;
-		}
-	}
+void suma(float num1, float num2) {
+    float total;
+    total = num1 + num2;
+    cout << "El resultado es: " << total;
 }
-//Funciones//
-void suma(float num1, float num2){
-	float total;
-	total = num1 + num2;
-	cout<<"El resultado es: "<<total;
+
+void resta(float num1, float num2) {
+    float total;
+    total = num1 - num2;
+    cout << "El resultado es: " << total;
 }
-void resta(float num1, float num2){
-	float total;
-	total = num1 - num2;
-	cout<<"El resultado es: "<<total;
+
+void multiplicacion(float num1, float num2) {
+    float total;
+    total = num1 * num2;
+    cout << "El resultado final es: " << total;
 }
-void multiplicacion(float num1, float num2){
-	float total;
- 	total = num1 * num2;
-	cout<<"El resultado final es:	"<<total;
+
+void division(float num1, float num2) {
+    float total;
+    total = num1 / num2;
+    cout << "El resultado final es: " << total;
 }
-void division(float num1, float num2){
-	float total;
- 	total = num1 / num2;
-	cout<<"El resultado final es:	"<<total;
-}
+
 float raiz(float num1) {
-    if (numero < 0) {
+    if (num1 < 0) {
         return -1.0;
     }
+    return sqrt(num1);
+}
 
-    float x0 = num1 / 2.0; 
-    float x1;
-
-    do {
-        x1 = (x0 + num1 / x0) / 2.0;
-    } while (abs(x1 - x0) >= 0.0001);
-
-    return x1;
+float exponente(float base, float exponente) {
+    return pow(base, exponente);
 }

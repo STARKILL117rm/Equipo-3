2678ea40bf455cedec5e881473e4171baad7bfce
Nahum Mejia <162595000+NahumMejia@users.noreply.github.com>
2024-03-07 08:46:34 -0600

    Update CALCULADORA.cpp

--- CALCULADORA.cpp
+++ CALCULADORA.cpp
@@ -4,7 +4,49 @@ using namespace std;
 struct calculadora {
 
   calculadora(){
-  
   }
+/*#include <iostream>
+using namespace std;
 
-
+void suma(float num1, float num2);
+void resta(float num1, float num2);
+main (){
+	int num1,num2;
+	char menu;
+		cout<<"--> Menu de la calculadora <---"<<endl;
+		cout<<"a) Suma"<<endl;
+		cout<<"b) Resta"<<endl;
+		cout<<"c) Multiplicacion"<<endl;
+		cout<<"d) Raiz cuadrada"<<endl;
+		cin>>menu;
+		switch (menu){
+		
+			case 'a':
+				cout<<"Dame el valor del numero 1: ";
+				cin>>num1;
+				cout<<"Dame el valor del numero 2: ";
+				cin>>num2;
+				suma(num1,num2);
+				break;
+			case 'b':
+				cout<<"Dame el valor del numero 1: ";
+				cin>>num1;
+				cout<<"Dame el valor del numero 2: ";
+				cin>>num2;
+				resta(num1,num2);
+				break;	
+		}
+	//}
+}
+//Funciones//
+void suma(float num1, float num2){
+	float total;
+	total = num1 + num2;
+	cout<<"El resultado es: "<<total;
+}
+void resta(float num1, float num2){
+	float total;
+	total = num1 - num2;
+	cout<<"El resultado es: "<<total;
+}
+*/
